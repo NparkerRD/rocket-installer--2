3aebeac17f474766d77f2e6ed6daa9608da040aa
unknown <nicole@rocketdaddy.ai>
2024-03-06 16:47:26 -0500

    add client form

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,3 +1,6 @@
+const overlay = document.querySelector(".overlay");
+
+// Navigation elements
 const navBtn = document.querySelector(".nav-btn");
 const burgerSpan = document.querySelector(".nav-btn__burger");
 const nav = document.querySelector(".nav");
@@ -6,24 +9,48 @@ const navCTA = document.querySelector(".nav__cta");
 const navItems = document.querySelectorAll(".nav__item");
 let displayMenu = false;
 
-const toggleNav = function () {
+// Form elements
+const addNewBtn = document.querySelector(".btn--add-new");
+const form = document.querySelector(".form");
+const cancelBtn = document.querySelector(".btn--cancel");
+
+const openOverlay = () => {
+  overlay.classList.add("open");
+};
+
+const closeOverlay = () => {
+  overlay.classList.remove("open");
+};
+
+// Open navigation
+const toggleNavigation = function (arr) {
   if (!displayMenu) {
-    burgerSpan.classList.add("open");
-    nav.classList.add("open");
-    navMenu.classList.add("open");
-    navCTA.classList.add("open");
+    arr.forEach((el) => el.classList.add("open"));
     navItems.forEach((item) => item.classList.add("open"));
 
     displayMenu = true;
   } else {
-    burgerSpan.classList.remove("open");
-    nav.classList.remove("open");
-    navMenu.classList.remove("open");
-    navCTA.classList.remove("open");
-    navItems.forEach((item) => item.classList.remove("open"));
+    arr.forEach((el) => el.classList.remove("open"));
+    navItems.forEach((el) => el.classList.remove("open"));
 
     displayMenu = false;
   }
 };
 
-navBtn.addEventListener("click", toggleNav);
+navBtn.addEventListener("click", () => {
+  toggleNavigation([burgerSpan, nav, navMenu, navCTA, overlay]);
+});
+
+// Open form
+addNewBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  openOverlay();
+  form.classList.add("open");
+});
+
+// Close form
+cancelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.classList.remove("open");
+  closeOverlay();
+});
